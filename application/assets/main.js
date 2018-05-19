306cb8e8dfc18159036eab1bd0a318779ea52c46
Tine Jozelj <tine.jozelj@tjo.space>
2018-05-19 21:27:29 +0200

    switch to mapbox

--- application/assets/main.js
+++ application/assets/main.js
@@ -1,28 +1,32 @@
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibWVudG9zMTM4NiIsImEiOiJjamhkcmg0ajMwbTdhMzZrZmllYXM0cmhiIn0.sWecb2oJ4lT9KnkbVot8iQ';
+
 (async () => {
 	// Hardcoded intial location to SF
 	const position = {lat: 37.7717378, lng: -122.4436925};
 
-	////
-	// Getting user location
-	// new Promise((resolve, reject) => {
-	// 	navigator.geolocation.getCurrentPosition(resolve, reject)
-	// }).then(pos => {
-	// 	console.log("Found user location", pos);
-	// 	position = {
-	// 		lat: pos.coords.latitude,
-	// 		lng: pos.coords.longitude,
-	// 	}
-	// })
 
 	////
 	// Google map
-	const mapElement = document.getElementById('map');
+	L.mapbox.accessToken = MAPBOX_ACCESS_TOKEN;
+	const map = L.mapbox.map('map', 'mapbox.streets')
+    	.setView([position.lat, position.lng], 13);
 
-	const map = new google.maps.Map(mapElement, {
-	  center: position,
-	  zoom: 12
-	});
+	// Create array of lat,lon points.
+	var line_points = [
+	    [37.7717378, -122.4436925],
+	    [37.7717374, -121.4436929],
+	];
 
+	// Define polyline options
+	// http://leafletjs.com/reference.html#polyline
+	var polyline_options = {
+	    color: '#000'
+	};
+
+	// Defining a polygon here instead of a polyline will connect the
+	// endpoints and fill the path.
+	// http://leafletjs.com/reference.html#polygon
+	var polyline = L.polyline(line_points, polyline_options).addTo(map);
 
 	$('#test-button').click(function () {
 		document.getElementById("map").classList.remove("blured");
