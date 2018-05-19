1a5215a71d1050defd3c56b9587a5adbbf50d4d7
dejvv <zagorsek.david@gmail.com>
2018-05-19 20:31:39 +0200

    Added blured map

--- application/assets/main.js
+++ application/assets/main.js
@@ -1,7 +1,15 @@
+window.onload = function(e){ 
+	// show starting modal
+	$('.modal').modal('show');
+    
+	const mapElement = document.getElementById('map');
 
-const mapElement = document.getElementById('map');
+	const map = new google.maps.Map(mapElement, {
+	  center: {lat: -34.397, lng: 150.644},
+	  zoom: 8
+	});
+}
 
-const map = new google.maps.Map(mapElement, {
-  center: {lat: -34.397, lng: 150.644},
-  zoom: 8
+$('#test-button').click(function () {
+	document.getElementById("map").classList.remove("blured");
 });
