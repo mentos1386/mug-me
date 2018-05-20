5c6131ba7339d7027d88dd38db93b3482115df17
Tine Jozelj <tine.jozelj@tjo.space>
2018-05-20 04:20:50 +0200

    code refactoring

--- application/assets/main.js
+++ application/assets/main.js
@@ -70,55 +70,29 @@
     //       strokeWeight: 2
     //     });
 
-    let i = 0;
    	let prevLatLang = "";
-
-   	// draw lines between coordinates
-    $.each(flightPlanCoordinates,function(k,v){
-    	i++;
-    	let curLatLang  = new google.maps.LatLng(v.lat, v.lng);
+    flightPlanCoordinates.forEach((value, key) => {
+    	const curLatLang  = new google.maps.LatLng(value.lat, value.lng);
+	    
 	    if(prevLatLang == "")
 	        prevLatLang = curLatLang;
-	    // else{
-        let strokeColor = makeGradientColor({r:0, g:255, b:204}, {r:255, g:0, b:0}, ((i/flightPlanCoordinates.length)*100));
+
+        const strokeColor = makeGradientColor(
+        	{r:0, g:255, b:204},
+        	{r:255, g:0, b:0},
+        	((key/flightPlanCoordinates.length)*100));
+
         console.log(prevLatLang, curLatLang, strokeColor.cssColor);
-        let flightPath = new google.maps.Polyline({
+
+        const flightPath = new google.maps.Polyline({
 	        path: [prevLatLang, curLatLang],
 	        geodesic: true,
 	        strokeColor: strokeColor.cssColor,
 	        strokeOpacity: 1.0,
 	        strokeWeight: 2
         });
+
         prevLatLang = curLatLang;
         flightPath.setMap(map);
-	    // }
-	});
-
-    // flightPath.setMap(map);
-
-	// calculate new color from existing colors
-	function makeGradientColor(color1, color2, percent) {
-	    var newColor = {};
-	    function makeChannel(a, b) {
-	        return(a + Math.round((b-a)*(percent/100)));
-	    }
-	    function makeColorPiece(num) {
-	        num = Math.min(num, 255);   // not more than 255
-	        num = Math.max(num, 0);     // not less than 0
-	        var str = num.toString(16);
-	        if (str.length < 2) {
-	            str = "0" + str;
-	        }
-	        return(str);
-	    }
-
-	    newColor.r = makeChannel(color1.r, color2.r);
-	    newColor.g = makeChannel(color1.g, color2.g);
-	    newColor.b = makeChannel(color1.b, color2.b);
-	    newColor.cssColor = "#" + 
-	                        makeColorPiece(newColor.r) + 
-	                        makeColorPiece(newColor.g) +
-	                        makeColorPiece(newColor.b);
-	    return(newColor);
-	}	
+    })
 })();
